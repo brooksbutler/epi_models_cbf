798b937664c6ab71bbf5ec039b01a58386bb3814
brooksbutler <30503735+brooksbutler@users.noreply.github.com>
2023-01-27 15:09:25 -0500

    actual initial commit

--- Model.py
+++ Model.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+
+def Laplacian(A):
+    L = np.zeros(A.shape)
+    n = len(A)
+    for i in range(n):
+        for j in range(n):
+            if i==j:
+                L[i,j] = np.sum(A[i,:])
+            else:
+                L[i,j] = -A[i,j]
+    return L
+
+def x_dot(x,Theta, B, U_l, V_l):
+    x_d = ((Theta + U_l) + np.kron(x.T,np.eye(len(x)))@(B-V_l))@x
+    return x_d 
+
+
+
